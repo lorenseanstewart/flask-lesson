ea051315d7e7a99dc6cf8d027a2541def9b61204
Loren Stewart <l.s.stewart111@gmail.com>
2021-04-03 16:26:59 -0700

    fixed import

--- app.py
+++ app.py
@@ -2,7 +2,7 @@ from flask import make_response, request, jsonify
 
 from models import User
 from startup import app, db
-from utils import serializer
+from utils import serialize_single, serialize_list
 
 
 # make a route that can (1) get all users or (2) create a new user
