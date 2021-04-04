50504393f587f032269fe0f242fe8e534990ac40
ian.f.stewart <ian.f.stewart@gmail.com>
2021-04-04 17:07:57 -0400

    first-route

--- app.py
+++ app.py
@@ -7,5 +7,9 @@ from utils import serialize_single, serialize_list
 
 # make a route that can (1) get all users or (2) create a new user
 
+@app.route('/users', methods=['GET'])
+def users():
+    if request.method == 'GET':
+        return jsonify('got it!')
 
 # create a route that gets a single user by id
